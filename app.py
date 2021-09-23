1a2ca7bc9022a97e613e693afa0e15a394c191e4
jamrod <myfavoritenoisemaker@gmail.com>
2021-09-23 14:37:23 -0600

    Tested all routes - ok

--- app.py
+++ app.py
@@ -1,35 +1,61 @@
 from flask import Flask, render_template, url_for, json, request
 from flask_cors import CORS
-# from flask_sqlalchemy import SQLAlchemy
 import os
+from uuid import uuid4
+
 
 
 data_url = os.path.join('./static/', 'volunteers.json')
 
 app = Flask(__name__)
 CORS(app)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-# db = SQLAlchemy(app)
 
-# class Volunteer(db.model):
-#     id = db.column(db.Integer, primary_key=True)
-#     isActive = db.Column(db.Boolean)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/volunteers', methods=['GET'])
+@app.route('/volunteers', methods=['GET', 'POST', 'DELETE', 'PUT'])
 def volunteers():
     with open(data_url) as file:
             data = json.load(file)
+
     if request.method == 'GET':
         return json.dumps(data)
+
     if request.method == 'POST':
-        pass
+        new_item = json.loads(request.json)
+        new_item["id"] = uuid4()
+        data.append(new_item)
+        with open(data_url, 'w') as write_file:
+            json.dump(data, write_file)
+        return json.dumps(new_item)
 
+    if request.method == 'DELETE':
+        id = json.loads(request.json)['id']
+        item_to_delete = None
+        for i, item in enumerate(data):
+            if item['id'] == id:
+                item_to_delete = i
+                break
+        deleted = data.pop(item_to_delete)
+        with open(data_url, 'w') as write_file:
+            json.dump(data, write_file)
+        return deleted
 
-
+    if request.method == 'PUT':
+        changes = json.loads(request.json)
+        print(changes)
+        id = changes['id']
+        item_to_change = None
+        for i, item in enumerate(data):
+            if item['id'] == id:
+                item_to_change = i
+                break
+        data[item_to_change] = changes
+        with open(data_url, 'w') as write_file:
+            json.dump(data, write_file)
+        return changes
 
 if __name__ == "__main__":
     app.run(debug=True)
